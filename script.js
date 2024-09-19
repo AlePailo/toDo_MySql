bcd49068540c4749264db76bd411ee072e218e87
alePailo <alepailo01@gmail.com>
2024-09-19 21:41:10 +0200

    updated base skeleton

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
+    $("#btnSend").click(function() {
+        createMemo($("#userInput").val())
+        console.log($(".addedElements p"))
+    })
+    $(".addedElements").on("click", "p", function(){
+        $(this).attr("contentEditable", "true")
+        $(this).focus()
+    })
+    $(".addedElements").on("blur", "p", function(){
+        $(this).attr("contentEditable", "false")
+    })
 })
+
+function createMemo(text) {
+    $('<p/>',{
+        text: text
+    }).attr("spellcheck", "false")
+    .appendTo('.addedElements')
+    $("#userInput").val("")
+}

605d66281d0c8f5498d11e5814cd818415736301
alePailo <alepailo01@gmail.com>
2024-09-20 22:04:44 +0200

    Polished UI

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 </head>
 <body>
     <section class="container">
-        <article class="settings">
-            <div>
-                <span class="material-symbols-outlined">arrow_back_ios</span>
+        <article id="navMenu">
+            <div id="navBackDiv">
+                <span id="backBtn" class="material-symbols-outlined">arrow_back_ios</span>
             </div>
-            <div>
-                <span class="material-symbols-outlined">delete</span>
-                <span class="material-symbols-outlined">settings</span>
+            <div id="navOptionsDiv">
+                <span id="deleteBtn" class="material-symbols-outlined">delete</span>
+                <span id="optionsBtn" class="material-symbols-outlined">settings</span>
             </div>
         </article>
         <form class="identification loginForm" action="#">
@@ -61,7 +61,6 @@
                 <span id="btnMic" class="material-symbols-outlined">mic</span>
                 <span id="btnSend" class="material-symbols-outlined">send</span>
             </div>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, tempore molestias! Voluptatibus unde voluptatem eligendi corrupti consequatur sunt, voluptatum sit eos porro ipsa harum iste facere animi voluptate, illum ipsam excepturi ea quam voluptas temporibus eveniet pariatur maiores! At nam corrupti, eos sed vitae officiis quae iusto natus? Iure sapiente, ullam adipisci, voluptatibus temporibus, deserunt dolores consectetur sit nulla molestias quidem ipsa. Mollitia fugit repellat laboriosam sunt similique tempora sequi excepturi blanditiis cum expedita facilis autem nesciunt, laudantium nobis? Error placeat mollitia dolor accusantium natus odit numquam harum magnam beatae vel. Et corporis at alias excepturi minima quae fuga dolorem aliquid ratione aspernatur, cum rem laudantium, ipsam porro magnam rerum nisi animi quia beatae. Aspernatur maxime distinctio placeat quasi ipsum cupiditate doloremque soluta, saepe fugiat sint accusantium. Omnis quis magni eum, quae nam non labore aliquid numquam iusto atque neque nostrum quo provident explicabo beatae? Alias labore earum accusantium perferendis natus quia quo esse dolores modi enim ducimus veniam quaerat, illum similique quibusdam id voluptate obcaecati nulla aut, porro minima explicabo? Facilis fugiat doloremque quia quae facere excepturi nisi minus laborum iure architecto assumenda similique reprehenderit hic, et quam eius dignissimos sit rem tempora ut id quibusdam dicta iusto? Hic eligendi similique ad iusto dicta, ratione quas corporis asperiores cupiditate pariatur odit illum consequatur eveniet rerum veritatis nemo perspiciatis dignissimos dolore accusamus, aspernatur quo velit eos suscipit! Sunt harum veniam, quod molestias quo voluptas. Fugit dicta atque corrupti hic, officia repudiandae facere earum unde ad maxime explicabo sint deserunt culpa incidunt a accusantium inventore autem commodi minus. Laborum, ipsa praesentium! Nisi nulla, libero, culpa quam voluptates iusto vel tempore doloribus minus saepe quod officia asperiores eius sed laboriosam earum ipsam quae repudiandae distinctio impedit sapiente. Ex deleniti dolorem quae architecto. Veniam reiciendis temporibus assumenda minus officia ab earum esse expedita commodi! Assumenda aperiam quibusdam quas exercitationem voluptatibus ipsum fuga dolorum commodi! Quod et maxime asperiores ab odit animi minima, alias sunt laborum laboriosam quae deserunt libero soluta est at doloribus tenetur repellendus nulla mollitia assumenda cupiditate sit odio dicta. Iure adipisci aliquid, voluptas nulla laudantium sunt! Fugiat, quas omnis? Quibusdam commodi doloribus amet, modi nobis dignissimos magnam? Tempora optio sequi dolor eveniet libero vero delectus voluptas autem, reprehenderit voluptatibus vitae odio natus nulla at quos perferendis id alias consequatur amet aspernatur explicabo ab omnis cumque praesentium! Incidunt quibusdam pariatur modi veritatis, provident animi quos cum odit dignissimos, tenetur perferendis fugit dolor fuga, accusamus iusto assumenda doloribus natus suscipit ex molestiae ipsum ad! Magni tempora soluta maxime ad nobis quisquam facilis cumque ipsam, est perferendis, impedit qui! Beatae ut sapiente perspiciatis repellendus fuga cum veniam perferendis earum, ipsum a laboriosam sed nisi dolore aut, repudiandae asperiores vitae vel delectus est enim ducimus quas libero magni exercitationem? Provident, culpa. Doloremque, numquam? Molestiae, consequatur, dignissimos et, beatae praesentium sequi eum nemo sunt quas repellat voluptatibus! Architecto optio dolorem aperiam provident ad enim incidunt pariatur amet libero, consequuntur cum esse expedita necessitatibus cupiditate, officia beatae a! Tempore odio deserunt fugiat nisi hic sint ipsa?</p>
         </article>
     </section>
 
--- script.js
+++ script.js
@@ -1,21 +1,108 @@
 $(document).ready(function() {
+
     $("#btnSend").click(function() {
         createMemo($("#userInput").val())
-        console.log($(".addedElements p"))
     })
+
     $(".addedElements").on("click", "p", function(){
         $(this).attr("contentEditable", "true")
         $(this).focus()
+        saveMemoValue($(this).text())
     })
+
     $(".addedElements").on("blur", "p", function(){
         $(this).attr("contentEditable", "false")
+        checkMemoValueVariations($(this).text())
     })
+
+    $(".addedElements").on("touchstart", ".memoNote", function() {
+        tapStart($(this))
+    })
+
+    $(".addedElements").on("touchend", ".memoNote", function() {
+        tapEnd($(this))
+    })
+
+    $("#backBtn").click(function() {
+        resetSelection()
+    })
+
+    
+    //localStorage.setItem("timer", null)
+
 })
+
+
+//MEMO ELEMENT CREATION BASED ON USER INPUT
 
 function createMemo(text) {
     $('<p/>',{
-        text: text
+        text: text,
+        class: "memoNote"
     }).attr("spellcheck", "false")
+    .attr("data-selected", "false")
     .appendTo('.addedElements')
     $("#userInput").val("")
+}
+
+
+
+
+// FIRST FUNCTION SAVES MEMO NOTE VALUE ON CLICK WHILE SECOND FUNCTION CHECKS IF THE VALUE IS CHANGED WHEN THE NOTE LOSES FOCUS
+// IF THE VALUE IS THE SAME NOTHING HAPPENS, IF IT'S DIFFERENT THE DATABASE GETS UPDATED WITH THE NEW MEMO VALUE
+
+function saveMemoValue(value) {
+    localStorage.setItem("memoValue", value)
+}
+
+
+function checkMemoValueVariations(currentValue) {
+    if(localStorage.getItem("memoValue") === currentValue) {
+        console.log("No variations")
+    } else {
+        console.log("Value changed")
+    }
+}
+
+
+let timer = null
+
+//LONG TAP LOGIC
+
+function tapStart(el) {
+    el.attr("data-selected", "true")
+    localStorage.setItem("longPress", false)
+    timer = setTimeout(() => {
+        timer = null
+        $("#backBtn").css("display", "inline-block")
+        $("#deleteBtn").css("display", "inline-block")
+        //el.attr("data-selected", "true")
+        console.log("half second passed")
+        localStorage.setItem("longPress", true)
+    }, 500)
+}
+
+function clearTimer() {
+    clearTimeout(timer)
+}
+
+function tapEnd(el) {
+    clearTimer()
+    console.log(localStorage.getItem("longPress"))
+    const isLongPressed = localStorage.getItem("longPress")
+    if(isLongPressed == "true") {
+        el.attr("data-selected", "true")
+    } else {
+        el.attr("data-selected", "false")
+    }
+    //localStorage.getItem("longPress") === true ? el.attr("data-selected", "true") : el.attr("data-slected", "false")
+    //el.attr("userSelected") === "true" ? el.css("background", "var(--on-focus-memo)") : el.css("background", "var(--secondary-bg)")
+}
+
+function resetSelection() {
+    $("#deleteBtn").css("display", "none")
+    $("#backBtn").css("display", "none")
+    $(".memoNote").attr("data-selected", "false")
+    //$(".memoNote").attr("userSelected") === "true" ? $(this).css("background", "var(--on-focus-memo)") : $(this).css("background", "var(--secondary-bg)")
+    //el.css("background", "var(--secondary-bg)")
 }

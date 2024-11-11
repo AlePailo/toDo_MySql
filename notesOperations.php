4a2c34b1f1f443f8f8ae249422f72bd1fbd207a7
alePailo <alepailo01@gmail.com>
2024-11-11 23:07:31 +0100

    Cleaned up php code

--- notesOperations.php
+++ notesOperations.php
@@ -1,21 +1,44 @@
 <?php
 
+//DB CONNECTION SETUP
 require_once("dbConnection.php");
 
-if(isset($_POST["deletionArr"])) {
-    deleteMemo($pdo, $_POST["deletionArr"]);
+
+//MEMO CREATION REQUEST
+if(isset($_POST["text"])) {
+    postMemo($pdo, $_POST["text"]);
 }
 
-if(isset($_GET["action"]) && $_GET["action"] == "loadContent") {
-    populateApp($pdo);
-}
 
+//MEMO CONTENT UPDATE REQUEST
 if(isset($_POST["newMemoText"])) {
     updateMemo($pdo, $_POST["newMemoText"], $_POST["changedMemoID"]);
 }
 
-if(isset($_POST["text"])) {
-    postMemo($pdo, $_POST["text"]);
+
+//MEMOS DELETION REQUEST
+if(isset($_POST["deletionArr"])) {
+    deleteMemo($pdo, $_POST["deletionArr"]);
+}
+
+
+//LOAD USER MEMOS REQUEST
+if(isset($_GET["action"]) && $_GET["action"] == "loadContent") {
+    populateApp($pdo);
+}
+
+
+
+//MEMO OPERATIONS
+function deleteMemo($pdo, $deletionArr) {
+    $parameters = str_repeat('?,', count($deletionArr) - 1) . '?';
+    $stmt = $pdo->prepare("DELETE FROM todos WHERE id IN ($parameters)");
+    $stmt->execute($deletionArr);
+}
+
+function updateMemo($pdo, $newText, $id) {
+    $stmt = $pdo->prepare("UPDATE todos SET content = ? WHERE id = ?");
+    $stmt->execute([$newText, $id]);
 }
 
 function postMemo($pdo, $text) {
@@ -32,27 +55,18 @@ function checkLastId($pdo) {
     echo json_encode($res);
 }
 
+
+//FILL PAGE WITH USERS MEMO
 function populateApp($pdo) {
     $userEmail = $_COOKIE["email"];
     $stmt = $pdo->prepare("SELECT content, id FROM todos WHERE user_email = ? ORDER BY pub_date");
     $stmt->execute([$userEmail]);
     $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(count($res) > 0) {
+    if(count($res)) {
         echo json_encode($res);
         return;
     }
     echo json_encode("No results");
-}
-
-function deleteMemo($pdo, $deletionArr) {
-    $parameters = str_repeat('?,', count($deletionArr) - 1) . '?';
-    $stmt = $pdo->prepare("DELETE FROM todos WHERE id IN ($parameters)");
-    $stmt->execute($deletionArr);
-}
-
-function updateMemo($pdo, $newText, $id) {
-    $stmt = $pdo->prepare("UPDATE todos SET content = ? WHERE id = ?");
-    $stmt->execute([$newText, $id]);
 }
 
 ?>
--- userOperations.php
+++ userOperations.php
@@ -1,69 +1,34 @@
 <?php
 
+//DB CONNECTION SETUP
 require_once("dbConnection.php");
 
-/*if(isset($_POST["regAttemptUsername"])) {
-    checkIfAlreadyExistsUser($pdo, $_POST["regAttemptUsername"]);
-}
 
-if(isset($_POST["regAttemptEmail"])) {
-    checkIfAlreadyExistsEmail($pdo, $_POST["regAttemptEmail"]);
-}
-
-if(isset($_POST["registrationEmail"])) {
-    registerUser($_POST["registrationUsername"], $_POST["registrationEmail"], $_POST["registrationPsw"], $pdo);
-}
-
-function checkIfAlreadyExistsUser($pdo, $field) {
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
-    $stmt->execute([$field]);
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(count($res) > 0) {
-        echo json_encode(true);
-    } else {
-        echo json_encode(false);
-    }
-    //return json_encode($res);
-}
-
-function checkIfAlreadyExistsEmail($pdo, $field) {
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
-    $stmt->execute([$field]);
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(count($res) > 0) {
-        echo json_encode(true);
-    } else {
-        echo json_encode(false);
-    }
-    //return json_encode($res);
-}
-
-function registerUser($username, $email, $psw, $pdo) {
-    $stmt = $pdo->prepare('INSERT INTO users(email, username, psw) VALUES (?,?,?)');
-    $stmt->execute([$email, $username, $psw]);
-}
-*/
-
+//LOGOUT REQUEST
 if(isset($_GET["action"]) && $_GET["action"] == "logout") {
     setcookie("username",  "", time() - 3600);
 }
 
+
+//CHECK IF USER IS LOGGED IN
 if(isset($_POST["GetSessionVariables"])) {
     checkLoginCookie();
 }
 
-function checkLoginCookie() {
-    echo json_encode(isset($_COOKIE["username"]));
-}
 
+//CHECK IF USERNAME OR EMAIL ALREADY EXISTS WHEN USER TRY TO REGISTER
 if(isset($_POST["regAttemptUsername"])) {
-    checkValidity($pdo, $_POST["regAttemptUsername"], $_POST["regAttemptEmail"], $_POST["regAttemptPsw"]);
+    checkRegistrationValidity($pdo, $_POST["regAttemptUsername"], $_POST["regAttemptEmail"], $_POST["regAttemptPsw"]);
 }
 
+
+//USER REGISTRATION REQUEST
 if(isset($_POST["registrationEmail"])) {
     registerUser($_POST["registrationUsername"], $_POST["registrationEmail"], $_POST["registrationPsw"], $pdo);
 }
 
+
+//LOGIN REQUEST
 if(isset($_POST["loginAttemptEmail"])) {
     if(checkLoginFormValidity($pdo, $_POST["loginAttemptEmail"], $_POST["loginAttemptPsw"]) != "valid") {
         echo json_encode("BOBBY");
@@ -73,7 +38,55 @@ if(isset($_POST["loginAttemptEmail"])) {
 }
 
 
-function checkValidity($pdo, $username, $email, $psw) {
+//CHECK IF USER COOKIE IS SET
+function checkLoginCookie() {
+    echo json_encode(isset($_COOKIE["username"]));
+}
+
+
+
+//TRY TO LOGIN AND SET COOKIES TO KEEP USER LOGGED IN
+function login($pdo, $email, $psw) {
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND psw = ?');
+    $stmt->execute([$email, $psw]);
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if(!count($res)) {
+        echo json_encode("Credenziali errate");
+        return;
+    }
+    setLoginCookie($res[0]["username"], $email);
+    echo json_encode("Logged in successfully");
+}
+
+function setLoginCookie($username, $email) {
+    setcookie("username", $username, time() + 60 * 60 * 24 * 365);
+    setcookie("email", $email, time() + 60 * 60 * 24 * 365);
+}
+
+
+
+//CHECK IN DATABASE IF USERNAME OR EMAIL ARE ALREADY REGISTERED
+function checkIfAlreadyExists($pdo, $fieldname, $fieldvalue) {
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE ' . $fieldname . '= ?');
+    $stmt->execute([$fieldvalue]);
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if(count($res)) {
+        return true;
+    }
+    return false;
+}
+
+//REGISTRATION QUERY
+function registerUser($username, $email, $psw, $pdo) {
+    $stmt = $pdo->prepare('INSERT INTO users(email, username, psw) VALUES (?,?,?)');
+    $stmt->execute([$email, $username, $psw]);
+    setLoginCookie($username, $email);
+}
+
+
+
+//FORMS VALIDATION
+function checkRegistrationValidity($pdo, $username, $email, $psw) {
     if(empty($username) || empty($email) || empty($psw)) {
         echo json_encode("Compilare tutti i campi");
         return;
@@ -84,12 +97,12 @@ function checkValidity($pdo, $username, $email, $psw) {
         return;
     }
 
-    if(checkIfAlreadyExistsEmail($pdo, $email) === true) {
+    if(checkIfAlreadyExists($pdo, "email" , $email) === true) {
         echo json_encode("Email in uso");
         return;
     }
 
-    if(checkIfAlreadyExistsUser($pdo, $username) === true) {
+    if(checkIfAlreadyExists($pdo, "username" , $username) === true) {
         echo json_encode("Username in uso");
         return;
     }
@@ -97,39 +110,6 @@ function checkValidity($pdo, $username, $email, $psw) {
     echo json_encode("Valid");
 }
 
-
-function checkIfAlreadyExistsEmail($pdo, $email) {
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
-    $stmt->execute([$email]);
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(count($res) > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function checkIfAlreadyExistsUser($pdo, $username) {
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
-    $stmt->execute([$username]);
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(count($res) > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function registerUser($username, $email, $psw, $pdo) {
-    $stmt = $pdo->prepare('INSERT INTO users(email, username, psw) VALUES (?,?,?)');
-    $stmt->execute([$email, $username, $psw]);
-    setLoginCookie($username, $email);
-}
-
-function setLoginCookie($username, $email) {
-    setcookie("username", $username, time() + 60 * 60 * 24 * 365);
-    setcookie("email", $email, time() + 60 * 60 * 24 * 365);
-}
 
 function checkLoginFormValidity($pdo, $email, $psw) {
     if(empty($email) || empty($psw)) {
@@ -141,19 +121,6 @@ function checkLoginFormValidity($pdo, $email, $psw) {
     }
 
     return "valid";
-}
-
-function login($pdo, $email, $psw) {
-    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND psw = ?');
-    $stmt->execute([$email, $psw]);
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(!count($res) > 0) {
-        echo json_encode("Credenziali errate");
-        return;
-    }
-    setLoginCookie($res[0]["username"], $email);
-    echo json_encode("Logged in successfully");
-    //echo "Hi " . $_COOKIE["username"] . " your email is " . $_COOKIE["email"];
 }
 
 ?>
